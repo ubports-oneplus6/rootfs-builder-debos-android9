063ee370dc5e0ed3590302f9197ce47c69f446e8
NeKit <nekit1000@gmail.com>
2020-06-21 15:08:02 +0200

    android9: install packages from xenial_-_gst-droid

--- scripts/add-android8-repos.sh
+++ scripts/add-android8-repos.sh
@@ -8,12 +8,17 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
 echo "deb http://repo.ubports.com/ xenial_-_edge_-_android8 main" >> /etc/apt/sources.list.d/ubports.list
+echo "deb http://repo.ubports.com/ xenial_-_gst-droid main" >> /etc/apt/sources.list.d/ubports.list
 
 echo "" >> /etc/apt/preferences.d/ubports.pref
 echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
-echo "Pin: origin repo.ubports.com" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin: release o=UBports,a=xenial_-_edge_-_android8" >> /etc/apt/preferences.d/ubports.pref
 echo "Pin-Priority: 2000" >> /etc/apt/preferences.d/ubports.pref
+
+echo "" >> /etc/apt/preferences.d/ubports.pref
+echo "Package: *" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin: release o=UBports,a=xenial_-_gst-droid" >> /etc/apt/preferences.d/ubports.pref
+echo "Pin-Priority: 1500" >> /etc/apt/preferences.d/ubports.pref
 
 apt update
 apt upgrade -y --allow-downgrades
