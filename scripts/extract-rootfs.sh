4d7e49952dc80141491fbd87cea54530d8333c2d
Caleb Connolly <caleb@connolly.tech>
2021-01-05 16:19:35 +0000

    Misc random changes

--- scripts/extract-rootfs.sh
+++ scripts/extract-rootfs.sh
@@ -8,4 +8,8 @@ image="$1"
 
 echo "Extracting rootfs..."
 dd if=$1 skip=17408 of=root-$1 iflag=skip_bytes,count_bytes
-echo "Rootfs image is $PWD/rootfs.img"
+mv root-$1 $1
+echo "Rootfs image is $1.img"
+echo "Shrinking rootfs"
+e2fsck -fy $1
+resize2fs -p $1
